92809b460440333bcb7012f6b3115d1be9b12a52
Yang Li <stephen.liy@gmail.com>
2014-12-01 17:06:31 -0500

    on going project

--- EmailSender/src/ca/foodpacker/tools/email/QueueBuilder.scala
+++ EmailSender/src/ca/foodpacker/tools/email/QueueBuilder.scala
@@ -1,25 +1,52 @@
 package ca.foodpacker.tools.email
-import java.sql.{Connection,DriverManager,SQLException}
-import org.apache.commons.dbutils.ResultSetHandler
+
+import java.sql.Connection
+import java.sql.DriverManager
 import java.sql.ResultSet
+import java.sql.SQLException
+
+import org.apache.commons.dbutils.QueryRunner
+import org.apache.commons.dbutils.ResultSetHandler
 
 object QueueBuilder {
 	def build:MailQueue={
 	  val ret=new MailQueue();
 	  //TODO: the queue will be built from the database
+	  var conn:Connection=null;
 	  try{
-	    val conn=DriverManager getConnection "jdbc:mysql://localhost/opencart?" +
+	    conn=DriverManager getConnection "jdbc:mysql://localhost/opencart?" +
 	    		"user=opencart&password=ilovesun"
 	    val h=new ResultSetHandler[Email](){
 	      @throws(classOf [SQLException])
 	      override def handle(rs:ResultSet):Email={
-	        return null;
+	        if(!rs.next()){
+	          return null; 
+	        }
+	        return new Email(
+	            rs.getInt("email_id"),
+	            rs.getString("to"),
+	            rs.getString("from"),
+	            rs.getString("cc"),
+	            rs.getString("title"),
+	            rs.getString("content")
+	            );
 	      }
 	    }
-	    
+	    val run=new QueryRunner();
+	    val emails=run.query[Email](conn,"SELECT * FROM oc_email where status=1",h);
+	    for(email <- emails)
+	    {
+	      
+	    }
 	  }catch
 	  {
 	    case e:SQLException => e.printStackTrace();
+	  }finally
+	  {
+	    if(conn!=null)
+	    {
+	      conn.close();
+	    }
 	  }
 	  
 	  ret < new Email(1,"liy@leesoft.ca",
